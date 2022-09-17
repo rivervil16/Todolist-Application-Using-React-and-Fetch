b6e6dd692ce9f6ee1a14d3228bf205eae4269093
rivervil16 <agustinriverovila@gmail.com>
2022-09-17 02:23:48 +0000

    copia de code de Todo List

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,68 @@
-import React from "react";
+import { element } from "prop-types";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+  const [item, setItem] = useState("");
+  const [list, setList] = useState([]);
+  const agregarTareas = (e) => {
+    e.preventDefault();
+    let tempList = [...list];
+    tempList.push(item);
+    setList(tempList);
+    setItem("");
+  };
+  const addItem = (e) => {
+    setItem(e.target.value);
+  };
+  const deleteItem = (indice) => {
+    let listDeleted = list.filter((itemTarea, posicion) => posicion !== indice);
+    setList(listDeleted);
+  };
 
+  return (
+    <div className="container">
+      <div className="container">
+        <h1 className="text-center">Todo List</h1>
+      </div>
+      <div className="col-4 mx-auto d-flex justify-content-between p-3">
+        <div className="col-12">
+          <form onSubmit={agregarTareas}>
+            <div className="d-flex justify-content-center ">
+              <div>
+                <input
+                  className="form-control"
+                  placeholder="agrega tu tarea aqui"
+                  type="text"
+                  value={item}
+                  onChange={addItem}
+                />
+              </div>
+              <button type="submit" className="btn-sm btn-outline-secondary">
+                Add
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className="col-4 mx-auto justify-content-between p-1 mb-2 bg-light text-dark border border-secondary rounded-3">
+        <div>
+          {list.map((element, indice) => (
+            <div className="d-flex justify-content-between m-1" key={indice}>
+              <p>{element}</p>
+              <button
+                className="btn btn-secondary"
+                onClick={() => deleteItem(indice)}
+              >
+                Borrar
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div>
+        <h5 className="text-center">Tareas Pendientes{" "}{list.length}</h5>
+      </div>
+    </div>
+  );
+};
 export default Home;
